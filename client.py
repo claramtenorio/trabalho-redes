4afd3cb88d28240e25cc50a89bab83fa46c6990f
claramtenorio <158226328+claramtenorio@users.noreply.github.com>
2025-06-23 12:16:50 -0300

    Update client.py
    para ficar de acordo com o utils

--- client.py
+++ client.py
@@ -1,48 +1,21 @@
 import socket
 import threading
-import math
-import datetime
-import struct
 import os
 import uuid #para gerar IDs das mensagens e nomes para os arquvivos temporários que serão usados
+import utils_client #importando as funcoes auxiliares do cliente
+#importando constanntes que não são definidas aqui
+from utils_constants import BUFFER_SIZE, HEADER_SIZE, MAX_SIZE_DATA, TYPE_HI, TYPE_BYE, TYPE_SEGMENT, TYPE_COMPLETE
 
-#variáveis globais
-client_socket = None
-username = ""
-connected = False
-client_ip = "0.0.0.0"
-client_port = 0
-#dicionario para reconstruir as mensagens que vem fragmentadas do servidor
-# { message_hash_id: {parts, received_count, total_fragments } }
-segments_from_server_buffer = {}
-
-#configuracoes do cliente
-SERVER_HOST = '127.0.0.1' #localhost, servidor e clientes na  mesma maquina
-SERVER_PORT = 65432 #porta alta, para eviter conflitos
-BUFFER_SIZE = 1024
-
-#tamanho do cabecalho
-#13 bytes = 1 byte de tipo da mensagem, 4 bytes do hash do id da mensagem,
-#4 bytes para o nem fragmento e 4 bytes para indicar o total de fragmento
-HEADER_SIZE = 13
-#tamanho maximo dos dados que podem ser enviados em uma mensagem
-MAX_DATA_SIZE = BUFFER_SIZE - HEADER_SIZE
-
-#tipos de mensagens, para o cabecalho
-#respectivamente - conectar, desconectar, msg fragmentada, msg toda
-TYPE_HI = 0
-TYPE_BYE = 1
-TYPE_SEGMENT = 2
-TYPE_COMPLETE = 3
 
 def client_main():
 
-    #definindo
-    global client_socket, username, connected, client_ip, client_port
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client_socket.bind(('0.0.0.0', 0)) 
-    client_ip, client_port = client_socket.getsockname()
-    print(f"novo cliente: {client_ip}:{client_port}")
+    #configura co contexto do cliente
+    utils_client.context_client.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    utils_client.context_client.SERVER_HOST = SERVER_HOST
+    utils_client.context_client.SERVER_PORT = SERVER_PORT
+    utils_client.context_client.client_socket.bind(('0.0.0.0', 0)) 
+    utils_client.context_client.client_ip, utils_client.context_client.client_port = utils_client.context_client.client_socket.getsockname()
+    print(f"Cliente iniciado em {utils_client.context_client.client_ip}:{utils_client.context_client.client_port}")
 
     #thread para receber mensagens do servidor
     receiving_thread = threading.Thread(target=receive_messages)
