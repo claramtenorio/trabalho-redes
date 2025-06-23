0ed3d5f3d43abd44c1af44cbf4d3e27e03f3e3b7
claramtenorio <158226328+claramtenorio@users.noreply.github.com>
2025-06-23 09:44:32 -0300

    Update client.py
    main do cliente

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ import uuid #para gerar IDs das mensagens e nomes para os arquvivos temporários
 #variáveis globais
 client_socket = None
 username = ""
-is_connected = False
+connected = False
 client_ip = "0.0.0.0"
 client_port = 0
 #dicionario para reconstruir as mensagens que vem fragmentadas do servidor
@@ -34,3 +34,53 @@ TYPE_HI = 0
 TYPE_BYE = 1
 TYPE_SEGMENT = 2
 TYPE_COMPLETE = 3
+
+def client_main():
+
+    #definindo
+    global client_socket, username, connected, client_ip, client_port
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket.bind(('0.0.0.0', 0)) 
+    client_ip, client_port = client_socket.getsockname()
+    print(f"novo cliente: {client_ip}:{client_port}")
+
+    #thread para receber mensagens do servidor
+    receiving_thread = threading.Thread(target=receive_messages)
+    receiving_thread.daemon = True
+    receiving_thread.start()
+
+    #entrada do usuário
+    while True:
+
+        #espera o usuário digitar algo (dar um input)
+        input_client = input("") 
+
+        #comeca com a mensagem de conexao
+        if input_client == 'hi, meu nome eh'
+            #define o nome como que na mesnagem de conexao, tirando os espacos
+            username = input_client[16:].strip()
+            hi_carga = f"{username}|{client_ip}|{client_port}".encode()
+            send_packet(hi_carga, 0, 0, 0, TYPE_HI)
+            connected = True
+            print(f"bem vindo(a) ao chat {username}!")
+        
+        #se o usuário digitar bye, desconecta
+        elif input_client == "bye":
+            send_packet(username.encode(), 0, 0, 0, TYPE_BYE)
+            connected = False
+            print("bye bye!")
+            break
+
+        #se o usuário digitar algo, envia a mensagem
+        elif connected:
+            send_to_chat(input_client)
+
+        #se o usuário não estiver conectado, avisa
+        else:
+            print("antes de se conversar, é preciso se conectar com o comando 'hi, meu nome eh <nome>'")
+
+    #close quando o loop para
+    if client_socket:
+        client_socket.close()
+
+client_main()
