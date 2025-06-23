e74cd2327c7df6a586f26c32cf596f1c9ff3628c
claramtenorio <158226328+claramtenorio@users.noreply.github.com>
2025-06-23 14:37:09 -0300

    Update server.py
    agora funcionando com seu utils

--- server.py
+++ server.py
@@ -1,43 +1,20 @@
 import socket
 import threading
-import math
-import datetime
-import struct
 import os
-import uuid #para gerar IDs das mensagens e nomes para os arquvivos temporários que serão usados
-
-#variáveis globais
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#dicionario para armazenar os clientes conectados
-#{ (ip, porta): nome_usuario }
-connected_clients = {}
-#dicionario para reconstruir as mensagens que vem fragmentadas do cliente
-#{ (ip, porta): { msg_id_hash, parts, received_count, total_fragments } }
-segments_from_client_buffer = {}
-
-#configuracoes do servidor
-SERVER_HOST = '0.0.0.0'  #ouve conexoes de qualquer interface de rede da maquia
-SERVER_PORT = 65432 #mesma porta do cliente
-BUFFER_SIZE = 1024
-
-#mesmo cabealho do cliente
-HEADER_SIZE = 13
-MAX_DATA_SIZE = BUFFER_SIZE - HEADER_SIZE
-
-#tipos de mensagens, para o cabecalho
-#respectivamente - conectar, desconectar, msg fragmentada, msg toda
-TYPE_HI = 0
-TYPE_BYE = 1
-TYPE_SEGMENT = 2
-TYPE_COMPLETE = 3
+import utils_server #importando as funcoes auxiliares do cliente
+#importando constanntes que não são definidas aqui
+from utils_constants import BUFFER_SIZE, HEADER_SIZE, MAX_SIZE_DATA, TYPE_HI, TYPE_BYE, TYPE_SEGMENT, TYPE_COMPLETE
 
 def server_main():
 
-    #inicio do socket do servidor
-    server_socket.bind((SERVER_HOST, SERVER_PORT))
+    #configuracao do servidor
+    utils_server.conext_server.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    utils_server.conext_server.SERVER_HOST = SERVER_HOST
+    utils_server.conext_server.SERVER_PORT = SERVER_PORT
+    utils_server.conext_server.server_socket.bind((SERVER_HOST, SERVER_PORT))
 
     while True:
-
+        #escuta e recebe pacotes
         datapacket, client_ad = server_socket.recvfrom(BUFFER_SIZE)
         #threads para processar os pacotes recebidos de usuarios diferentes
         threading.Thread(target=treat_packets_received, args=(datapacket, client_ad)).start()
